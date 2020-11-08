dd772123d32e2b8ec8c52818f10646d5388b9228
Augusto Martinez <acm_asd@hotmail.com>
2020-11-08 13:59:04 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 //JS script
+
+$(document).ready(function(){
+  
+  $("form").submit(function(e){
+    e.preventDefault;
+  });
+  
+  function addItem() {
+    let $ul;
+    let $li;
+    let $label;
+    let $div;
+    let value;
+    
+    value = $(".js-new-item").val().trim();
+    
+    // Empty values
+    if (value.length == 0) {
+      return;
+    }
+    
+    $ul = $("ul");
+    $li = $("<li>").appendTo($ul);
+    
+    $div = $("<div>")
+          .addClass("checkbox")
+          .appendTo($li);
+          
+    $label = $("<label>").appendTo($div);
+    
+    $("<input>")
+    .attr("type", "checkbox")
+    .addClass("item")
+    .attr("name", "list")
+    .click(checkedItem)
+    .appendTo($label);
+    
+    $label.append(value);
+    
+    $(".js-new-item").val("");    
+  }
+  
+  function checkedItem(e) {
+    let $el;
+    
+    $el = $(e.currentTarget);
+    $el.closest("li").toggleClass("removed");
+  }
+  
+  $(".js-add").click(addItem);
+  $(".js-item").click(checkedItem);
+  
+});
